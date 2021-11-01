795310aa203c43aeaf4dd70326259b4e8722214d
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-11-01 19:41:21 +0100

    cleared unused things

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,8 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+
 import Form from "../../components/Form/Form";
 import TaskList from "../../components/TaskList/TaskList";
 import useTasks from "../../hooks/useTasks";
-
-const tasks = [
-  {
-    id: "1",
-    title: "do homework",
-    description: "clean bedroom",
-    time_frame: "15:00-16:00",
-  },
-  {
-    id: "2",
-    title: "do this",
-    description: "clean loo",
-    time_frame: "15:00-16:00",
-  },
-  {
-    id: "4",
-    title: "do that",
-    description: "clean that",
-    time_frame: "15:00-16:00",
-  },
-];
 
 const Home = () => {
   const { tasks, loadTasks } = useTasks();
