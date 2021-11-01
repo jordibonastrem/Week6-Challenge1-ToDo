1f4d44ce7cb104d3d2d29a7352e4b679d49eb83f
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-11-01 21:15:52 +0100

    add form events

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [title, setTitle] = useState();
+  const [desciption, setDescription] = useState();
   const handleSubmit = (event) => {
+    console.log(title);
+    console.log(desciption);
     event.preventDefault();
+  };
+  const handleOnChange = (event) => {
+    if (event.target.id === "description") {
+      setDescription(event.target.value);
+    } else {
+      setTitle(event.target.value);
+    }
   };
 
   return (
     <div>
-      <form onSubmit={(event) => handleSubmit(event)}>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="title">Title:</label>
-        <input type="text" id="title" name="title" />
+        <input type="text" id="title" name="title" onChange={handleOnChange} />
         <label htmlFor="description">Description:</label>
-        <input type="text" id="description" name="description" />
+        <input
+          type="text"
+          id="description"
+          name="description"
+          onChange={handleOnChange}
+        />
         <input type="submit" value="Submit" />
       </form>
     </div>
