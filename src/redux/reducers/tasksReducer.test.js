71ad3f283fbc6519d4b5ff9b0b0e3fb7de1ca1cd
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-11-02 19:31:28 +0100

    aded create test tasks

--- src/redux/reducers/tasksReducer.test.js
+++ src/redux/reducers/tasksReducer.test.js
@@ -1,5 +1,6 @@
 import { getRandomTasks, getRandomTask } from "../../factories/tasksFactory";
 import actionTypes from "../actions/actionTypes";
+import taskReducer from "./tasksReducer";
 
 describe("Given a tasksReducer reducer", () => {
   describe("When it receives an empty tasks list and a load action with three tasks", () => {
@@ -11,6 +12,22 @@ describe("Given a tasksReducer reducer", () => {
         type: actionTypes.loadTasks,
         tasks: tasksList,
       };
+      const newTaskList = taskReducer(initialTasks, action);
+      expect(newTaskList).toEqual(tasksList);
+    });
+  });
+  describe("When it receives a tasks list and a create action with a new task", () => {
+    test("Then it should return a new task list including the new task", () => {
+      const initialTasks = getRandomTasks(3);
+      const newTask = getRandomTask();
+      const action = {
+        type: actionTypes.createTask,
+        task: newTask,
+      };
+
+      const newTaskList = taskReducer(initialTasks, action);
+
+      expect(newTaskList).toContainEqual(newTask);
     });
   });
 });
