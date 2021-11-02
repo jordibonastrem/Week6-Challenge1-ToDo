907312d848a552c9913bf1d5bed91b821dbe2319
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-11-02 18:50:58 +0100

    added load and create actionCreators tests

--- src/redux/actions/actionCreator.test.js
+++ src/redux/actions/actionCreator.test.js
@@ -1,5 +1,34 @@
+import { getRandomTask, getRandomTasks } from "../../factories/tasksFactory";
+import { createTaskAction, loadTasksAction } from "./actionCreators";
+import actionTypes from "./actionTypes";
+
 describe("Given a load actionCreator", () => {
-  describe("When it receives a list of challenges", () => {
-    test("Then it should return a load type action with the challenges received", () => {});
+  describe("When it receives a list of tasks", () => {
+    test("Then it should return a load type action with the tasks received", () => {
+      const tasksList = getRandomTasks();
+      const expectedAction = {
+        type: actionTypes.loadTasks,
+        tasks: tasksList,
+      };
+
+      const actionResult = loadTasksAction(tasksList);
+      expect(actionResult).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a create actionCreator", () => {
+  describe("When it receives a task", () => {
+    test("Then it should return a create type action with the task received", () => {
+      const newTask = getRandomTask();
+      const expectedAction = {
+        type: actionTypes.createTask,
+        task: newTask,
+      };
+
+      const actionResult = createTaskAction(newTask);
+
+      expect(actionResult).toEqual(expectedAction);
+    });
   });
 });
